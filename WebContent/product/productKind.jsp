eb6bef21681dcaf5f0f3ea45363795ed196d4ee7
hjkim <pjt911219@gmail.com>
2020-09-18 10:24:56 +0900

    c

---
+++ WebContent/product/productKind.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+  pageEncoding="UTF-8"%>  
+<%@ include file="../header.jsp" %>  
+<%@ include file="sub_img.html"%> 
+<%@ include file="sub_menu.html" %>       
+  <article>
+    <h2> Item</h2>     
+    <c:forEach items="${productKindList }"  var="productVO">
+      <div id="item">
+        <a href="ProductDetail.do?no=${productVO.no}"> 
+          <img src="product_images/${productVO.image}" />
+          <h3>${productVO.name} </h3>        
+          <p>${productVO.salePrice} </p>
+        </a>  
+      </div>
+    </c:forEach>    
+    <div class="clear"></div>
+  </article>
+<%@ include file="../footer.jsp" %>    
---
+++ src/nonageShop/controller/model/ProductKindModel.java
@@ -0,0 +1,30 @@
+package nonageShop.controller.model;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import nonageShop.controller.Command;
+import nonageShop.dto.Product;
+import nonageShop.service.ProductService;
+
+public class ProductKindModel implements Command {
+	private ProductService service = new ProductService();
+	
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		System.out.println("오나");
+		String kind = request.getParameter("kind").trim();
+		ArrayList<Product> list = service.listKindProduct(kind);
+		request.setAttribute("productKindList", list);
+		response.sendRedirect("product/productKind.jsp");
+		return null;
+	}
+
+}
